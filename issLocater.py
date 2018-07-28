92b5d85041d83e3392d8dd8a1ee1a0200105db21
SaintGemini <chacone96@yahoo.com>
2018-07-28 12:37:17 -0700

    initial commit
    added .gitignore

---
+++ issLocater.py
@@ -0,0 +1,41 @@
+import json
+import turtle
+import urllib.request
+from threading import *
+import time
+
+
+astronauts = 'http://api.open-notify.org/astros.json'
+response = urllib.request.urlopen(astronauts)
+result = json.loads(response.read())
+names = result['people']
+print('People in Space', result['number'])
+for p in names:
+    print(p['name'])
+
+def repeatISS():
+    while(True):
+        isslocation = 'http://api.open-notify.org/iss-now.json'
+        response2 = urllib.request.urlopen(isslocation)
+        endresult = json.loads(response2.read())
+        location = endresult['iss_position']
+        lat = location['latitude']
+        lon = location['longitude']
+        print('Latitude: ', lat)
+        print('Longitude: ', lon)
+        time.sleep(5)
+
+t = Timer(5.0, repeatISS)
+t.start()
+
+
+
+
+
+
+#image = "worldmap.jpg"
+
+#screen = turtle.Screen()
+#screen.setup(720, 360)
+#screen.setworldcoordinates(-180, -90, 180, 90)
+#screen.bgpic("space.jpg")
